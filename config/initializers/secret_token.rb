24d173beb95966c331ff6d217736c3f7f8f8bd43
DJ <jebekkim@gmail.com>
2011-11-22 14:52:13 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ehammy::Application.config.secret_token = 'fc474e00087c20c58a68561514b25aa51013e6b96b4600ac220581372ad5360b153ce4c15cf36b1e8a6f638d841dfc29fa892fb8c98196a454b66dba19638f10'
